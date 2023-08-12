6e5ab81b3240e27c39d328121836ecdd00216571
Michael Loughman <mcloughman@gmail.com>
2023-08-12 15:47:01 -0700

    Add delete functionality to HikeContext and HikeDetails. Test in app. State again is updated locally and we don't have to refresh. It updates in our browser.

--- src/components/HikeDetails.jsx
+++ src/components/HikeDetails.jsx
@@ -1,4 +1,21 @@
+import { useHikesContext } from "../hooks/useHikesContext"
+import { FaTrashAlt } from "react-icons/fa"
+
 const HikeDetails = ({ hike }) => {
+  const { dispatch } = useHikesContext()
+  const handleClick = async () => {
+    const response = await fetch(
+      "http://localhost:4000/api/hikes/" + hike._id,
+      {
+        method: "DELETE",
+      }
+    )
+    const json = await response.json()
+    if (response.ok) {
+      dispatch({ type: "DELETE_HIKE", payload: json })
+    }
+  }
+
   return (
     <div className="hike-object">
       <div className="hike-details">
@@ -10,6 +27,8 @@ const HikeDetails = ({ hike }) => {
         </p>
       </div>
       <img src={hike.images[0]} alt="stunning scenery" />
+
+      <button onClick={handleClick}>Delete</button>
       <hr />
     </div>
   )
